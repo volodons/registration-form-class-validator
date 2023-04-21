12675ec9490542b2037efe71f7c5dae1314fb2fe
Volodymyr Donets <volodons@gmail.com>
2023-04-21 19:05:27 +0300

    Create Validator class + Regex + add interactivity to email input and submit button

--- task2_4/scripts/script.js
+++ task2_4/scripts/script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const submitButton = document.querySelector("#submit");
+submitButton.addEventListener("click", isEmail);
+const emailInput = document.querySelector("#email");
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+class Validator {
+  email;
+
+  constructor(params) {
+    this.email = params.email;
+  }
+
+  isEmail(string) {
+    if (emailRegex.test(string)) {
+      alert("This looks like an E-Mail, yay!");
+    } else {
+      alert("This is not an E-Mail, get out of my building!");
+    }
+  }
+}
