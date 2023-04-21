c41d685d330896537de03ce4767d81c8aec0d242
Volodymyr Donets <volodons@gmail.com>
2023-04-21 21:05:25 +0300

    Add code to validate password

--- task2_4/scripts/script.js
+++ task2_4/scripts/script.js
@@ -6,20 +6,35 @@ submitButton.addEventListener("click", submit); // ADD SUBMIT EVENT
 class Validation {
   constructor(email) {
     this.email = email;
+    this.password = password;
   }
 
   isEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (emailRegex.test(email)) {
-      alert("This looks like an E-Mail!");
+      alert("Email confirmed");
     } else {
-      alert("This is not an E-Mail!");
+      alert("Please check your e-mail input again!");
+    }
+  }
+
+  isPassword(password) {
+    const passwordRegex =
+      /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    if (passwordRegex.test(password)) {
+      alert("Password confirmed");
+    } else {
+      alert(
+        "Your password should have at least 8 upper and lower case letters, at least 1 symbol, at least 1 number"
+      );
     }
   }
 }
 
 function submit() {
   const email = document.querySelector("#email").value;
+  const password = document.querySelector("#password").value;
   const form = new Validation(email);
   form.isEmail(email);
+  form.isPassword(password);
 }
