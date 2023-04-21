baf24ea824a975705f3c48e916bc0f2b40367b87
Volodymyr Donets <volodons@gmail.com>
2023-04-21 20:05:37 +0300

    Add submit function to validate input (email, in this case)

--- task2_4/scripts/script.js
+++ task2_4/scripts/script.js
@@ -1,22 +1,25 @@
 "use strict";
 
 const submitButton = document.querySelector("#submit");
-submitButton.addEventListener("click", isEmail);
-const emailInput = document.querySelector("#email");
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+submitButton.addEventListener("click", submit); // ADD SUBMIT EVENT
 
-class Validator {
-  email;
-
-  constructor(params) {
-    this.email = params.email;
+class Validation {
+  constructor(email) {
+    this.email = email;
   }
 
-  isEmail(string) {
-    if (emailRegex.test(string)) {
-      alert("This looks like an E-Mail, yay!");
+  isEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (emailRegex.test(email)) {
+      alert("This looks like an E-Mail!");
     } else {
-      alert("This is not an E-Mail, get out of my building!");
+      alert("This is not an E-Mail!");
     }
   }
+}
+
+function submit() {
+  const email = document.querySelector("#email").value;
+  const form = new Validation(email);
+  form.isEmail(email);
 }
