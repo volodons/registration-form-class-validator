09402b1b2a7d08846818c29d7de4100f024046bb
Volodymyr Donets <volodons@gmail.com>
2023-04-22 20:16:29 +0300

    Refactor code: make alert messages more intuitive and user-friendly

--- task2_4/scripts/script.js
+++ task2_4/scripts/script.js
@@ -1,8 +1,7 @@
 "use strict";
 
 const submitButton = document.querySelector("#submit");
-submitButton.addEventListener("click", submit); // ADD SUBMIT EVENT
-
+submitButton.addEventListener("click", submit);
 class Validation {
   constructor(
     firstName,
@@ -27,7 +26,7 @@ class Validation {
   isFirstName(firstName) {
     const firstNameRegex = /^[A-Za-z\s]+$/;
     if (firstNameRegex.test(firstName)) {
-      alert("First name confirmed");
+      alert("First name is confirmed. Processing...");
     } else {
       alert(
         "Your first name should have only A-Z and a-z letters, spaces can also be used"
@@ -38,7 +37,7 @@ class Validation {
   isLastName(lastName) {
     const lastNameRegex = /^[A-Za-z\s]+$/;
     if (lastNameRegex.test(lastName)) {
-      alert("Last name confirmed and processed");
+      alert("Last name is confirmed. Processing...");
     } else {
       alert(
         "Your last name should have only A-Z and a-z letters, spaces can also be used"
@@ -49,9 +48,9 @@ class Validation {
   isEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (emailRegex.test(email)) {
-      alert("Email confirmed");
+      alert("E-Mail is confirmed. Processing...");
     } else {
-      alert("Please check your e-mail input again!");
+      alert("Your e-mail input is not an e-mail!");
     }
   }
 
@@ -59,7 +58,7 @@ class Validation {
     const passwordRegex =
       /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
     if (passwordRegex.test(password)) {
-      alert("Password processed");
+      alert("Password is acquired. Moving onto confirmation...");
     } else {
       alert(
         "Your password should have at least 8 upper and lower case letters, at least 1 symbol, at least 1 number"
